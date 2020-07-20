0f7a7bef3356444995072f78bf704a9d03fcb461
somuknight <46134066+somuknight@users.noreply.github.com>
2020-07-20 15:12:59 +0530

    improved a little bit

--- src/Substraction.java
+++ src/Substraction.java
@@ -5,7 +5,6 @@ public class Addition{
         Scanner sc= new Scanner(System.in);
         int a= sc.nextInt();
         int b= sc.nextInt();
-        int c=b-a;
-        System.out.println(c);
+        System.out.println(b-a);
     }
 }
